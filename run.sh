22ccaf8f16e888266c31b04f2b0c66741418f1de
Jackie Jin <liveaeon@gmail.com>
2013-02-19 17:50:53 -0800

    configuration shell update

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-cp .emacs ~/.emacs
-cp -r ./.emacs.d ~/.emacs.d
+cp dotEmacs ~/.emacs
+sed "s@\/home\/jackie\/@$HOME\/@g" ~/.emacs
+cp -r emacs.d ~/.emacs.d
 
+echo "" >> ~/.bashrc
 echo "export TERM=xterm-256color" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "alias emacs=\"emacs -nw\"" >> ~/.bashrc
+echo "configuration complete!"
 . ~/.bashrc
