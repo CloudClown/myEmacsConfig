ca6f87b3c3da23b07a62f542f956ec981ddd8912
Jackie Jin <liveaeon@gmail.com>
2013-08-17 20:07:39 -0700

    minor tweak

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cp dotEmacs ~/.emacs
-cp -r emacsd/* ~/.emacs.d
 
 wget -P ~/.emacs.d/ http://ftp.gnu.org/gnu/tramp/tramp-2.0.39.tar.gz 
 tar -xzvf ~/.emacs.d/tramp-2.0.39.tar.gz -C ~/.emacs.d/
@@ -11,4 +10,5 @@ echo "export TERM=xterm-256color" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo "alias emacs=\"emacs -nw\"" >> ~/.bashrc
 echo "configuration complete!"
+echo "please run \"cp -r emacsd/* ~/.emacs.d/\" to complete setup"
 source ~/.bashrc
