4817b3fd68d4896ca896343ef8cb0edf01cbefdd
Jackie Jin <liveaeon@gmail.com>
2014-04-12 02:36:07 -0700

    run.sh modified

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ echo "export TERM=xterm-256color" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo "alias emacs=\"emacs -nw\"" >> ~/.bashrc
 echo "configuration complete!"
-echo "please run \"cp -r emacsd/* ~/.emacs.d/\" to complete setup"
-source ~/.bashrc
+echo "please run \"cp -r emacsd/* ~/.emacs.d/\" to copy required files"
+echo "pleas run \"source ~/.bashrc\" to complete setup"
