f6e97354201a8a460cf3bdacbab446b5b6ed3f9a
Jackie Jin <liveaeon@gmail.com>
2014-06-24 14:17:19 -0700

    reduce unnecessary tools

--- run.sh
+++ run.sh
@@ -3,9 +3,6 @@
 cp dotEmacs ~/.emacs
 cp -r emacsd/* ~/\.emacs\.d
 
-wget -P ~/.emacs.d/ http://ftp.gnu.org/gnu/tramp/tramp-2.0.39.tar.gz 
-tar -xzvf ~/.emacs.d/tramp-2.0.39.tar.gz -C ~/.emacs.d/
-
 echo "" >> ~/.bashrc
 echo "export TERM=xterm-256color" >> ~/.bashrc
 echo "" >> ~/.bashrc
